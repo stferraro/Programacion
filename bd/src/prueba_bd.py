ead2651cdb67445195377676f61860edd63b9e33
stferraro <gerferr83@gmail.com>
2024-05-09 01:34:26 -0400

    bew_bd

--- bd/src/prueba_bd.py
+++ bd/src/prueba_bd.py
@@ -1,18 +1,41 @@
 import psycopg2
 
-conexion = psycopg2.connect(
-    user = 'gerardo',
-    password = 'desarrollo',
-    host = '127.0.0.1',
-    port = '5432',
-    database = 'pruebadb'
-)
+conexion = psycopg2.connect(user = 'gerardo', 
+                            password = 'desarrollo', 
+                            host = 'localhost', 
+                            port = '5432', 
+                            database = 'pruebadb'
+                        )
 
-cursor = conexion.cursor()
-sentencia = 'select * from persona;'
-cursor.execute(sentencia)
-registros = cursor.fetchall()
-print(registros)
-cursor.close()
-conexion.close()
 
+#conexion sencilla
+try :
+    cursor = conexion.cursor()
+    cursor.execute('SELECT * FROM persona')
+    for persona in cursor.fetchall():
+        print(persona)
+
+except Exception as e:
+    print(e)
+
+finally:
+    cursor.close()
+    conexion.close()
+    
+#conexion con with
+try:
+    with psycopg2.connect(user = 'gerardo', 
+                            password = 'desarrollo', 
+                            host = 'localhost', 
+                            port = '5432', 
+                            database = 'pruebadb'
+                        ) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute('SELECT * FROM persona')
+            for persona in cursor.fetchall():
+                print(persona)
+
+except Exception as e:
+    print(e)
+finally:
+    conn.close()
