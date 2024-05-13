f315affcea26dce1efa2c81693cf5379dc644575
stferraro <gerferr83@gmail.com>
2024-05-13 02:26:25 -0400

    add

--- universidad/main.py
+++ universidad/main.py
@@ -5,13 +5,47 @@ from src.schema.user_schema import UserSchema
 
 app = FastAPI()
 connection = Connection()
-print(connection)
 
 
 @app.get("/")
 async def root():
-    connection
-    return {"message": "Hello World"}
+    items = []
+    for row in connection.read_all():
+        dictionary = {}
+        dictionary['id'] = row[0]
+        dictionary['nombres'] = row[1]
+        dictionary['apellidos'] = row[2]
+        dictionary['cedula'] = row[3]
+        dictionary['email'] = row[4]
+        dictionary['telefono'] = row[5]
+        items.append(dictionary)
+    return items
+
+@app.get("/api/estudiante/{id}")
+async def read_by_id(id: str):
+    items = {}
+    row = connection.read_by_id(id)
+    if row:
+        items['id'] = row[0]
+        items['nombres'] = row[1]
+        items['apellidos'] = row[2]
+        items['cedula'] = row[3]
+        items['email'] = row[4]
+        items['telefono'] = row[5]
+    return items
+
+@app.delete("/api/delete/{id}")
+async def delete(id: str):
+    connection.delete(id)
+    return {"message": "El registro se eliminó correctamente"}
+
+ 
+@app.put("/api/update/{id}")
+def update(id: str, user_data: UserSchema):
+    data = user_data.model_dump()
+    data['id'] = id
+    connection.update(data)
+ 
 
 @app.post("/api/insert")
 def insert(user_data: UserSchema):
--- universidad/src/model/connection.py
+++ universidad/src/model/connection.py
@@ -30,7 +30,31 @@ class Connection:
             sentence = """INSERT INTO estudiantes (nombres, apellidos, cedula, email, telefono) VALUES (%(nombres)s, %(apellidos)s, %(cedula)s, %(email)s, %(telefono)s)"""
             cur.execute(sentence, data) 
         self.conn.commit()
-            
+        
+    def read_all(self):
+        with self.conn.cursor() as cur:
+            sentence = """SELECT * FROM estudiantes"""
+            cur.execute(sentence)
+            return cur.fetchall()
+        
+    def read_by_id(self, id):
+        with self.conn.cursor() as cur:
+            sentence = """SELECT * FROM estudiantes WHERE id = %(id)s"""
+            cur.execute(sentence, {'id': id})
+            return cur.fetchone()
+        
+    def delete(self, id):
+        with self.conn.cursor() as cur:
+            sentence = """DELETE FROM estudiantes WHERE id = %(id)s"""
+            cur.execute(sentence, {'id': id})
+        self.conn.commit()
+        
+    def update(self, data):
+        with self.conn.cursor() as cur:
+            sentence = """UPDATE estudiantes SET nombres = %(nombres)s, apellidos = %(apellidos)s, cedula = %(cedula)s, email = %(email)s, telefono = %(telefono)s WHERE id = %(id)s"""
+            cur.execute(sentence, data)
+        self.conn.commit()
+        
                          
     def __def__(self):
         self.conn.close() #destructor
